ff9aaf5543c6490fdeb3f2c7a532e178afb7e47f
jae yeong <chujy1224@gmail.com>
2023-12-21 00:35:48 +0900

    - share 기능 수정  1) Web share API 가 지원되지 않는 Chrome 브라우저 대응 	:  공유 버튼을 클릭 시 링크 공유 형식으로 기능 변경하기 위해 share.js 코드 수정
    - index.html 코드 수정 (title 등)

--- js/share.js
+++ js/share.js
@@ -1,23 +1,38 @@
-// 기능2: 공유하기
-document.getElementById('shareButton').addEventListener('click', async () => {
-    try {
-        // Check if the Web Share API is available
-        if (!navigator.share) {
-            alert('Web Share API is not available on your browser.');
-            return;
-        }
+// 기능2: 공유하기 (원본 코드)
+// Chrome 브라우저에서는 URL copy 방식으로 공유하도록 코드 수정 
 
-        // Your share data
-        const shareData = {
+document.getElementById('shareButton').addEventListener('click', () => {
+    // Check if the navigator.share is available
+    if (navigator.share) {
+        navigator.share({
             title: 'Web Share Example',
             text: 'Check out this web share example!',
             url: window.location.href
-        };
+        })
+            .then(() => console.log('Data was shared successfully'))
+            .catch((err) => console.error('Share failed:', err.message));
+    } else {
+        // Web Share API 지원하지 않는 브라우저에서는 shareURL 을 통해 URL 복사 붙여넣기 방식 활용 
+        const shareURL = 'https://chujaeyeong.github.io/Advent-Calender/'; // github 배포 링크 추가
+        const shareText = shareURL;
 
-        // Invoke the share dialog
-        await navigator.share(shareData);
-        console.log('Data was shared successfully');
-    } catch (err) {
-        console.error('Share failed:', err.message);
+        // Create a temporary textarea element
+        const shareDialog = document.createElement('textarea');
+        shareDialog.value = shareText;
+        document.body.appendChild(shareDialog);
+
+        // Select the text in the textarea
+        shareDialog.select();
+
+        try {
+            // Copy the selected text to the clipboard
+            document.execCommand('copy');
+            alert('링크가 복사되었습니다. 원하는 곳에 붙여넣기 하세요!');
+        } catch (err) {
+            console.error('Unable to copy to clipboard:', err);
+        } finally {
+            // Remove the temporary textarea element
+            document.body.removeChild(shareDialog);
+        }
     }
 });
